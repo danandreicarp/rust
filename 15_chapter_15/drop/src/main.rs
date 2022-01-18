82579879e742fdaf4fbc88051ce8474d0970815f
danandreicarp <carpdan@pm.me>
2022-01-18 05:48:58 +0200

    chapter 15 code

---
+++ 15_chapter_15/drop/src/main.rs
@@ -0,0 +1,25 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointers created");
+    // c.drop(); // explicit use of destructor method
+
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main");
+}
